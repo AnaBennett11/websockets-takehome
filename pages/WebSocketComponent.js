b4fc52f3081088fa8a475416f876c4ec45de49c5
Ana Bennett <anabennett1111@gmail.com>
2023-07-08 12:05:41 -0500

    Import websocket component in the index and display messages

--- pages/WebSocketComponent.js
+++ pages/WebSocketComponent.js
@@ -2,11 +2,29 @@ import React, { useEffect, useState } from "react";
 import WebSocket from "isomorphic-ws";
 
 const WebSocketComponent = () => {
-    return (
-        <div>
+  const [events, setEvents] = useState([]);
 
+  useEffect(() => {
+    const socket = new WebSocket("ws://beeps.gg/stream");
+    socket.onmessage = (event) => {
+      const eventData = JSON.parse(event.data);
+      setEvents((prevEvents) => [eventData, ...prevEvents]);
+    };
+    return () => {
+      socket.close();
+    };
+  }, []);
+
+  return (
+    <div>
+      {events.map((event) => (
+        <div key={event.id}>
+          <p>{event.message}</p>
+          {/* Display other event details as desired */}
         </div>
-    )
-}
+      ))}
+    </div>
+  );
+};
 
 export default WebSocketComponent;
--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,21 @@
 import React, { useState } from "react";
-
+import WebSocketComponent from "./WebSocketComponent";
 
 const Home = () => {
   const [eventCount, setEventCount] = useState(0);
   const [eventRate, setEventRate] = useState(0);
 
-
-return (
+  return (
     <div>
       <div className="sidebar">
         <p>Event Count: </p>
         <p>Event Rate (per minute): </p>
       </div>
-   
+        <div className="main">
+          <WebSocketComponent />
+        </div>
     </div>
   );
-}
+};
 
 export default Home;
