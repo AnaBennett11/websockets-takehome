824093a3b7e8c9fa08178d349518834d9ed1c2c5
Ana Bennett <anabennett1111@gmail.com>
2023-07-08 11:53:06 -0500

    Create websocket component

---
+++ pages/WebSocketComponent.js
@@ -0,0 +1,12 @@
+import React, { useEffect, useState } from "react";
+import WebSocket from "isomorphic-ws";
+
+const WebSocketComponent = () => {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default WebSocketComponent;
